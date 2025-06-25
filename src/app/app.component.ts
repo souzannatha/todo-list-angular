b7151e960b7052573e5952bc9d2b6e107dbab928
souzannatha <souzanatha04@gmail.com>
2025-06-25 17:02:47 -0300

    feat: added base ui component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './_components/header/header.component';
+import { TaskComponent } from './_components/task/task.component';
+import { BaseUiComponent } from './_components/base-ui/base-ui.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, TaskComponent, BaseUiComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
